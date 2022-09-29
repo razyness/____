0e576555158e18ecb03a7c6432cfa80c4af3e69c
Razy <97766343+razyness@users.noreply.github.com>
2022-09-29 22:54:45 +0200

    _
    ___________________

--- _.py
+++ _.py
@@ -1,4 +1,6 @@
 import _ as _
+import os
+import time
 from __ import ___
 
 ____ = _._(_, '_')
@@ -17,3 +19,37 @@ for _ in ____:
 
 if __name__ == '____':
   _._____()
+
+  
+____ = '__'
+
+___ = _.____.__()
+
+
+__ = _______.___(___________=____, ___=______)
+
+
+@__._____()
+def ____(___):
+    if '_' in _._____ and _.____.____ is False:
+        _._______('_')
+    _.______(___)
+
+
+def underscore():
+    for _ in os.listdir('.\_'):
+        if _.endswith('.py'):
+            __.____(f'_.{_}', True, _.______)
+        else:
+            print(_ + '___')
+            
+    if _.lower() == '_':
+            _._____('underscore', True)
+
+    elif _.____ in _:
+        ___ = _.____._('____')
+        time.sleep(2)
+        return _
+
+
+asyncio.run(____())
